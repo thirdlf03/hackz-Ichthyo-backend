52602d7fea6ca7228837815461779538a0645295
yagi <s22a1154@bene.fit.ac.jp>
2025-09-17 00:48:48 +0900

    PUT付け加えたよ

--- routers/player.py
+++ routers/player.py
@@ -2,10 +2,24 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db import get_db
 from models.player import Player
+from schemas.player import PlayerSchema
 
 router = APIRouter()
 
-@router.get("/player")
-def get_player(db: Session = Depends(get_db)):
-    players = db.query(Player).all()
-    return players
+
+@router.get("/player") 
+def get_db(db: Session = Depends(get_db)):
+    return db.query(Player).all()
+
+@router.post("/player")
+def create_player(player: PlayerSchema, db: Session= Depends(get_db)):
+    new_db = Player(id = Player.id, money = Player.money)
+    db.add(new_db)
+    db.commit()
+    return db.query(Player).all()
+
+@router.put("/player/money")
+async def update_money(player_id: int, player: PlayerSchema, db: Session = Depends(get_db)):
+    player_update_money = player_money + service_money
+
+    return db.query(Player).all()
