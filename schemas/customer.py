efd3fd8c8aeb59dc5768f0205da6a74a40315542
Rozpring <s24a1070@bene.fit.ac.jp>
2025-09-16 17:31:46 +0900

    customerスキーマを定義した

---
+++ schemas/customer.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Customer(BaseModel):
+    id: int
+    name: str
+    money: int = 0
+    profile: Optional[str] = None
+    level: int = 1
+    status: str = "active"
