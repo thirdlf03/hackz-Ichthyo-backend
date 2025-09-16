2a00548a3ee7f7ebd1fc536350109f6182711cc1
Rozpring <s24a1070@bene.fit.ac.jp>
2025-09-16 22:52:42 +0900

    GET/customersの修正

--- routers/customer.py
+++ routers/customer.py
@@ -1,12 +1,15 @@
 import random
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 from db import get_db
 from models.customer import Customer
 
 router = APIRouter()
 
+
 @router.get("/customer")
-def get_customer(db: Session = Depends(get_db)):
-    customers = db.query(Customer).all()
+async def get_customer(db: Session = Depends(get_db)):
+    results = await db.execute(select(Customer))
+    customers = results.scalars().all()
     return random.choice(customers)
