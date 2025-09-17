90ab93213b2c382bf2ad5640388fc00420cf51be
Rozpring <s24a1070@bene.fit.ac.jp>
2025-09-17 10:28:35 +0900

    chat履歴の取得

--- routers/customer.py
+++ routers/customer.py
@@ -1,12 +1,34 @@
 import random
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import select, text
 from db import get_db
-from fastapi import APIRouter
-from models import customer
-from  schemas import customer
+from models.customer import Customer
 
 router = APIRouter()
 
 @router.get("/customer")
-def get_customer(db: Session = Depends(get_db)):
-	customers=db.query(customer).all()
-    return random.choice(customers)
+async def get_customer(db: Session = Depends(get_db)):
+    sql = """
+    SELECT * FROM customer ORDER BY RAND() LIMIT 1;
+    """
+
+    result = await db.execute(text(sql))
+    customer = result.fetchone()
+
+    if customer is None:
+        return {"error": "No customer found"}
+
+    return customer._asdict()
+
+@router.get("/customers/{customer_id}/chat")
+async def get_customer_chat(customer_id: int, db: Session = Depends(get_db)):
+    chat_sql = """
+        SELECT message FROM chat WHERE customer_id = :customer_id_param;
+        """
+
+    chat_result = await db.execute( text(chat_sql), {"customer_id_param": customer_id} )
+    chat = chat_result.scalars().all()
+
+    return chat
+
